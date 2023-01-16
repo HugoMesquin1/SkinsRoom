f3ec22acb4ebd1bb3648792d01ce3d66bc374603
Hugo Mesquini <hugomesq1n1@gmail.com>
2023-01-16 10:43:45 -0300

    first commit

---
+++ src/modules/itens/useCases/findByType/FindByTypeController.ts
@@ -0,0 +1,17 @@
+import { FindByTypeUseCase } from "./FindByTypeUseCase"
+import { Request, Response } from "express";
+
+
+
+export class FindByTypeController {
+    async handle(request: Request, response: Response) {
+        const type_skin = request.query.params
+        
+        const findByTypeUseCase = new FindByTypeUseCase()
+        
+        const filter = await findByTypeUseCase.execute(type_skin)
+
+        return response.json(filter)
+        
+    }
+}
