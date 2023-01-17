0a1357251c9670705e0251aa212894c5625ec643
Hugo Mesquini <hugomesq1n1@gmail.com>
2023-01-17 17:40:52 -0300

    refactor: Correção de GetItemUseCase

--- src/modules/buyer/useCases/getItem/GetItemUseCase.ts
+++ src/modules/buyer/useCases/getItem/GetItemUseCase.ts
@@ -5,20 +5,43 @@ import { prisma } from "../../../../database/prismaClient"
 
 export class GetItemUseCase {
     async execute(id: string){
-        const GetInfo = await prisma.item.findFirst({
+        const GetInfo = await prisma.item.findUnique({
+          
             where:{
-                id: id
+                id: id,
             },
+            
+            
+            
+            
             
             include:{
                 salesman: {
-                    select: {
-                        username: true,
-                        contact: true
-                        
-                    }
-                }
+                },
+                
+                    
+                
             }
         })
-        return GetInfo
+
+        const ItemConfirmation = {
+            
+            type_skin: GetInfo?.item_name,
+            item_name: GetInfo?.item_name,
+            price: GetInfo?.price,
+            float_item: GetInfo?.float_item,
+
+
+        }
+
+        const ContactSeller = {
+            username: GetInfo?.salesman?.username,
+            contact: GetInfo?.salesman?.contact
+        }
+            
+            
+
+
+            
+        return {ItemConfirmation, ContactSeller}
     }}
